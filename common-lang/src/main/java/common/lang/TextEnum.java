4a05d3556b59caf0da36aedbd29feb541157241d
zhongyuan zhang <zzy.hit@gmail.com>
2019-03-26 16:05:28 +0800

    customized TextEnum name

--- common-lang/src/main/java/common/lang/TextEnum.java
+++ common-lang/src/main/java/common/lang/TextEnum.java
@@ -4,15 +4,20 @@ import com.google.common.collect.Maps;
 
 import java.util.Collections;
 import java.util.Map;
+import java.util.function.Function;
 
 public interface TextEnum {
 
     String text();
 
     static <T extends TextEnum> Map<T, String> toMap(T[] values) {
+        return toMap(values, TextEnum::text);
+    }
+
+    static <T extends TextEnum> Map<T, String> toMap(T[] values, Function<TextEnum, String> function) {
         Map map = Maps.newLinkedHashMapWithExpectedSize(values.length);
         for (T value : values) {
-            map.put(value, value.text());
+            map.put(value, function.apply(value));
         }
         return Collections.unmodifiableMap(map);
     }
